0acdb0399b1049b636c981776d154325044b5529
Jaroslaw Gawel <audikrk@interia.pl>
2022-01-04 22:54:12 +0100

    Dołożony skrypt LockCameraZ który blokuje podazanie kamery za graczem w osi X,dzieki czemu blokujemy kamery w tej osi dodając add extension--> LockCameraZ do CM vcam1

---
+++ Assets/Scripts/Player/LockCameraZ.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[ExecuteInEditMode] [SaveDuringPlay] [AddComponentMenu(" ")]
+public class LockCameraZ : CinemachineExtension
+{
+    public float CamXPositionLock = 0;
+
+    protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+    {
+       if (stage == CinemachineCore.Stage.Body)
+        {
+            var pos = state.RawPosition;
+            pos.x = CamXPositionLock;
+            state.RawPosition = pos;
+        }
+    }
+}
