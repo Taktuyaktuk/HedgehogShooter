ac659ff27b0d08f059255b36b8342eed2ae441e4
Jaroslaw Gawel <audikrk@interia.pl>
2022-01-07 02:34:59 +0100

    Wstępna wersja przeciwnika Lis, do dopracowania

--- Assets/Scripts/Enemies/Fox/EnemyFox.cs
+++ Assets/Scripts/Enemies/Fox/EnemyFox.cs
@@ -2,16 +2,113 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyFox : MonoBehaviour
+public class EnemyFox : EnemyAbstract
 {
-    // Start is called before the first frame update
-    void Start()
+    public float Damage = 5f;
+    public float HP = 100f;
+    public float Cooldown = 1.0f;
+    public float Speed = 2f;
+    private Transform target;
+
+    public Transform DodgeRightPoint;
+    public Transform DodgeLeftPoint;
+    public Transform DodgeEndPoint;
+
+
+    private enum state
+    {
+        //follow,
+        //zigZag,
+        //atacking,
+        dodgeStart,
+        dodgeRight,
+        dodgeLeft,
+        dodgeEnd
+    }
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        Move();
+        Die();
+    }
+    public override void Attack()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Dash()
+    {
+        
+    }
+
+    public override void Die()
+    {
+       if(HP <=0)
+        {
+            Destroy(gameObject); 
+        }
+    }
+
+    public override void GetDamage()
+    {
+        
+    }
+
+    public override void Idle()
+    {
+        
+    }
+
+    public override void Move()
+    {
+        float dist = Vector3.Distance(target.position, transform.position);
+        Transform lastPosition;
+        Transform lastPlayerPosition;
+
+        if (dist <= 3.5f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, Speed * Time.deltaTime);
+        }
+        //else if ( dist > 3.5f)
+        //{
+            //int rnd = Random.Range(1, 3);
+            //switch (state)
+            //{
+            //    default:
+            //    case state.dodgeStart:
+            //        lastPosition = DodgeRightPoint;
+            //        transform.position = Vector3.MoveTowards(transform.position, lastPosition.position, Speed * Time.deltaTime);
+            //        if(transform.position == lastPosition)
+            //        {
+
+            //        }
+            //        break;
+            //}
+            
+
+            //if (transform.position != DodgeRightPoint.position)
+            //{
+            //    transform.position = Vector3.MoveTowards(transform.position, DodgeRightPoint.position, Speed * Time.deltaTime);
+            //}
+            //else if (rnd == 2)
+            //{
+            //    transform.position = Vector3.MoveTowards(transform.position, target.position, Speed * Time.deltaTime);
+            //}
+            //else if (rnd == 3)
+            //{
+            //    target.di
+            //}
+        //}
+        
+        
+    }
+
+    public override void Respawn()
     {
         
     }
